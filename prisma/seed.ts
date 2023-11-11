96adfb893691b477d7613c6a2ab7ead7ab800a1c
Florencea <bearflorencea@gmail.com>
2023-11-11 18:00:30 +0800

    feat: update db for group

--- prisma/seed.ts
+++ prisma/seed.ts
@@ -9,13 +9,26 @@ const DEFAULT_USER = {
   website: "admin@admin.com",
 };
 
+const DEFAULT_GROUP = {
+  code: "G000",
+  name: "administrator",
+  editable: false,
+};
+
 async function main() {
+  const defaultGroup = await prisma.group.upsert({
+    where: {
+      code: DEFAULT_GROUP.code,
+    },
+    create: DEFAULT_GROUP,
+    update: DEFAULT_GROUP,
+  });
   await prisma.user.upsert({
     where: {
       account: DEFAULT_USER.account,
     },
-    create: DEFAULT_USER,
-    update: DEFAULT_USER,
+    create: { ...DEFAULT_USER, groupPid: defaultGroup.pid },
+    update: { ...DEFAULT_USER, groupPid: defaultGroup.pid },
   });
 }
 main()
